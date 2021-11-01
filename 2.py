ace6d82d6fa597031ea961056c04771fa9af1e2b
TsungWing <38560218+W0n9@users.noreply.github.com>
2021-11-01 11:48:42 +0800

    update dependment

--- 2.py
+++ 2.py
@@ -3,7 +3,7 @@ from matplotlib.font_manager import FontProperties
 
 from Refl_calc import Refl_calc
 
-chinese_font = FontProperties(fname="/usr/share/fonts/source/SourceHanSans-Normal.ttc")
+# chinese_font = FontProperties(fname="/usr/share/fonts/source/SourceHanSans-Bold.ttc")
 
 if __name__ == "__main__":
     x = []
@@ -13,8 +13,9 @@ if __name__ == "__main__":
         R.append(Refl_calc(_ / 1000))
 
     plt.figure(figsize=(12, 9))
-    plt.plot(x, R)
-    plt.title("反射率R随MgF2薄膜光学厚度变化的曲线", fontproperties=chinese_font)
-    plt.xlabel("λ0", fontproperties=chinese_font)
-    plt.ylabel("反射率R", fontproperties=chinese_font)
+    plt.plot(x, R, "b", linewidth=5.0)
+    plt.title("Reflectivity with the optical thickness of MgF2")
+    plt.xlabel("λ0")
+    plt.ylabel("Reflectivity")
+    plt.savefig("fig.jpg", pad_inches=0)
     plt.show()
